77912f6aa1e6e06047e7b918e553c8fcc861a262
Wang96 <nwang@colgate.edu>
2016-02-23 16:16:15 -0500

    Update stop.java
    start and stop should not have messages, these should be in serverStarted and serverStopped hook methods.

--- src/SimpleChatServer/stop.java
+++ src/SimpleChatServer/stop.java
@@ -16,9 +16,9 @@ public class stop extends ServerCommand {
 		if (!getServer().isClosed()){
 			getServer().setClosed(true);
 			getServer().handleMessageFromUser("Server has stopped listening.");	
-			getServer().stopListening();
-			getServer().serverUI().display("Server has stopped listening.");
-		}
+			getServer().serverStopped();
+			//getServer().serverUI().display("Server has stopped listening.");
+			}
 		else 
 			System.out.println("The server is already closed. No action was performed.");
 	}
