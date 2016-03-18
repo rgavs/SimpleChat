f40e7911e3eb418e9e4bc6a84c04f5bd6090bbf4
ryan@Archer <gavigan.ryan@gmail.com>
2016-03-18 16:57:46 -0400

    something seems wrong... will checkout master and return to this branch

--- src/client/login.java
+++ src/client/login.java
@@ -4,24 +4,49 @@ import common.*;
 import java.io.IOException;
 
 /**
- * Implements client command to log in to the current host.
+ *  Implements client command to log in to the current host.
  *
  * @author Chris Nevison
  * @version February 2012
+ * 
+ * Modified by Shouheng to allow for password protection, and to change the 
+ * error message in connectionException()
+ * February 28 2016
  */
-public class login extends NotConnectedClientCommand {
-	public login(String str, ChatClient1 client) {
-		super(str, client);
-	}
+public class login extends NotConnectedClientCommand
+{
+  public login(String str, ChatClient1 client)
+  {
+    super(str, client);
+  }
 
-	public void doCmd() {
-		try {
-			getClient().openConnection();
-			getClient().clientUI().display("Connection to " + getClient().getHost() + " opened.");
-			getClient().sendToServer(new ServerLoginHandler(getClient().getId()));
-		} catch (IOException ex) {
-			getClient().clientUI().display("Connection to " + getClient().getHost() + " failed.");
-		}
-	}
-
+  public void doCmd()
+  {	  
+	  int indexOfBlank = getStr().indexOf(' ');
+	  
+	  if(indexOfBlank == -1) {
+		  getClient().clientUI().display("Incorrect username or password.");
+		  return;
+	  } // end if
+	  String username = getStr().substring(0, indexOfBlank);
+	  String password = getStr().substring(indexOfBlank+1);
+      
+	  if(username.equals("")) {
+		  getClient().clientUI().display("Incorrect username or password.");
+		  return;
+	  }
+	  
+	  //check if password consists of blanks only
+	  
+    try
+    {
+      getClient().openConnection();
+      getClient().sendToServer(new ServerLoginHandler(getClient().getId(), getClient().getPassword()));
+    }
+    catch(IOException ex)
+    {
+      getClient().clientUI().display("Connection to " + getClient().getHost() + " failed.");
+    }
+  }
 }
+
